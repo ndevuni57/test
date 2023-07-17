ad59355f42ca78e5f1b2e4debd64ea5123c8b2d9
ndevuni57 <ndevuni57@gmail.com>
2023-07-17 20:45:47 +0530

    merge commit1

--- src/main/java/com/example/learnmerge/mergebranch/TestMerge.java
+++ src/main/java/com/example/learnmerge/mergebranch/TestMerge.java
@@ -1,4 +1,10 @@
 package com.example.learnmerge.mergebranch;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TestMerge {
+    public void getService(){
+        System.out.println("mergeMethod");
+    }
 }

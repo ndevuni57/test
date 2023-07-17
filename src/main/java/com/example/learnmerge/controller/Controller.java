2122cfc2aa70e4f634f8a82b3f4eb92b38dabd1b
ndevuni57 <ndevuni57@gmail.com>
2023-07-17 20:42:36 +0530

    controller commit1

--- src/main/java/com/example/learnmerge/controller/Controller.java
+++ src/main/java/com/example/learnmerge/controller/Controller.java
@@ -1,6 +1,13 @@
 package com.example.learnmerge.controller;
 
-@RestControlelr
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
-    
+
+    @GetMapping
+    public void getMethod(){
+        System.out.println("controller method");
+    }
 }

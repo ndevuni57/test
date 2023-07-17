d237e5e8e7994a3215c3c5e1fce3b759f19feb6a
ndevuni57 <ndevuni57@gmail.com>
2023-07-17 21:20:53 +0530

    merge branch1

--- src/main/java/com/example/learnmerge/mergebranch/TestMerge.java
+++ src/main/java/com/example/learnmerge/mergebranch/TestMerge.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestMerge {
     public void getService(){
-        System.out.println("mergeMethod");
+        System.out.println("mergeMethod123 - merge branch");
     }
 }
